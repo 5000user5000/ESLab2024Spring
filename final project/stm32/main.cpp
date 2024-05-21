45cb5eb0395c216b2024e90bc39d63aafbdba34b
t30t <71395346+t30t@users.noreply.github.com>
2024-05-21 16:28:42 +0800

    Timer版本

--- final project/stm32/main.cpp
+++ final project/stm32/main.cpp
@@ -2,25 +2,53 @@
 #include "accelero.h"
 #include "network.h"
 #include <sstream>
+#include <chrono>
+
+using events::EventQueue;
+using std::chrono::milliseconds;
 
 constexpr char HOSTNAME[] = "192.168.50.200";
+constexpr milliseconds EVENT_INTERVAL = 100ms;
 
-int main() {
-    DigitalOut led1(LED1);
-    DigitalOut led2(LED2);
-    AcceleroSensor sensor;
-    WifiSocket socket(HOSTNAME, 8080);
+static EventQueue eventQueue(32*EVENTS_EVENT_SIZE);
+static AcceleroSensor* pSensor;
+static WifiSocket* pSocket;
+static Ticker timer;
+static DigitalOut led1(LED1);
+static InterruptIn button(BUTTON1);
+
+void stopSendingData() {
+    timer.detach();
+    delete pSocket;
+    led1 = 0;
+}
+
+void sendSensorData() {
+    if(pSocket == nullptr) return;
+    if(!pSocket->isAvailable()) {
+        stopSendingData();
+        return;
+    }
 
     stringstream stringBuilder;
-    while(socket.isAvailable()) {
-        stringBuilder.clear();
 
-        sensor.update();
-        stringBuilder << sensor.getX() << "|" << sensor.getY() << "|" << sensor.getZ();
-        socket.sendData(stringBuilder.str());
+    pSensor->update();
+    stringBuilder << "{\"x\":" << pSensor->getX() << ",\"y\":" << pSensor->getY() << ",\"z\":" << pSensor->getZ() << "}";
+    pSocket->sendData(stringBuilder.str());
+}
 
-        ThisThread::sleep_for(100ms);
-    }
+int main() {
+    pSensor = new AcceleroSensor();
+    pSocket = new WifiSocket(HOSTNAME, 8080);
+    timer.attach([] {
+        eventQueue.call(sendSensorData);
+    }, EVENT_INTERVAL);
+    button.rise([] {
+        eventQueue.call(stopSendingData);
+    });
+
+    led1 = 1;
+    eventQueue.dispatch_forever();
 
     return 0;
 }
